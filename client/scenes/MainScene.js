237237dd33ef80718787fbd610c6ba9ebada2db2
Carly <dipasqualecarly@gmail.com>
2021-02-04 12:36:21 -0500

    bugfix: asset loading size

--- client/scenes/MainScene.js
+++ client/scenes/MainScene.js
@@ -1,21 +1,21 @@
-import "phaser";
+import 'phaser';
 
 export default class MainScene extends Phaser.Scene {
-  constructor() {
-    super("MainScene");
-    this.state = {};
-  }
-  preload() {
-    this.load.image("officePlan", "assets/backgrounds/officePlan.png");
-  }
-  create() {
-    const scene = this;
+	constructor() {
+		super('MainScene');
+		this.state = {};
+	}
+	preload() {
+		this.load.image('officePlan', 'assets/backgrounds/officePlan.png');
+	}
+	create() {
+		const scene = this;
 
-    //background
-    this.add.image(0, 0, "officePlan").setOrigin(0);
+		//background
+		this.add.image(400, 300, 'officePlan').setOrigin(0);
 
-    //
-    // this.socket = io();
-  }
-  update() {}
+		//
+		// this.socket = io();
+	}
+	update() {}
 }
