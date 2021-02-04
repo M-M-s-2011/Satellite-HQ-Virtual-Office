5a399531dc721f83c36b98e0f3ff3b182834e99c
LB <laurabradburn27@gmail.com>
2021-02-04 12:54:55 -0500

    feat(add banner):MainScene

--- client/scenes/MainScene.js
+++ client/scenes/MainScene.js
@@ -1,21 +1,24 @@
-import 'phaser';
+import "phaser";
 
 export default class MainScene extends Phaser.Scene {
-	constructor() {
-		super('MainScene');
-		this.state = {};
-	}
-	preload() {
-		this.load.image('officePlan', 'assets/backgrounds/officePlan.png');
-	}
-	create() {
-		const scene = this;
+  constructor() {
+    super("MainScene");
+    this.state = {};
+  }
+  preload() {
+    this.load.image("officePlan", "assets/backgrounds/officePlan.png");
+    this.load.image("banner", "assets/backgrounds/banner.png");
+  }
+  create() {
+    const scene = this;
 
-		//background
-		this.add.image(400, 300, 'officePlan').setOrigin(0);
+    //background
+    this.add.image(400, 300, "officePlan");
+    const banner = this.add.image(400, 50, "banner");
 
-		//
-		// this.socket = io();
-	}
-	update() {}
+    banner.setScale(0.4);
+    //
+    // this.socket = io();
+  }
+  update() {}
 }
