99c48f7c612a138f3211a14e6617951d644d23ab
Carly <dipasqualecarly@gmail.com>
2021-02-05 13:09:56 -0500

    chore: event listeners

--- client/socket/game.js
+++ client/socket/game.js
@@ -1,4 +1,35 @@
 //front end event listeners for game
-const connectGame = (io) => {};
+const joinRoom = (socket, gameRooms, roomKey) => {
+	socket.join(roomKey);
+	const roomInfo = gameRooms[roomKey];
+	console.log('roomInfo', roomInfo);
+	roomInfo.players[socket.id] = {
+		rotation: 0,
+		x: 400,
+		y: 300,
+		playerId: socket.id,
+	};
+	//update number of players
+	roomInfo.numPlayers = Object.keys(roomInfo.players).length;
+
+	//send the players object to the new player
+	socket.emit('currentPlayers', {
+		players: roomInfo.players,
+		numPlayers: roomInfo.numPlayers,
+	});
+
+	// update all other players of the new player
+	//  socket.to(roomKey).emit("newPlayer", {
+	//   playerInfo: roomInfo.players[socket.id],
+	//   numPlayers: roomInfo.numPlayers,
+	// });
+};
+
+//connect event listeners
+const connectGame = (io, gameRooms) => {
+	io.on('connection', (socket) => {
+		socket.on('joinRoom', (roomKey) => joinRoom(socket, gameRooms, roomKey));
+	});
+};
 
 export default connectGame;
--- client/socket/index.js
+++ client/socket/index.js
@@ -1,10 +1,18 @@
 //connect front end event listeners to Socket
-import connectVideo from "./video";
-import connectGame from "./Game";
+import connectVideo from './video';
+import connectGame from './Game';
+
+const gameRooms = {
+	office: {
+		// users: [],
+		players: {},
+		numPlayers: 0,
+	},
+};
 
 const connect = (io) => {
-  connectVideo(io);
-  connectGame(io);
+	connectVideo(io);
+	connectGame(io, gameRooms);
 };
 
 export default connect;
