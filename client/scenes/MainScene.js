897d87440a37894f63f52fb1cae266aed744fa16
Gracie Ingermanson <gmi@umich.edu>
2021-02-07 16:56:07 -0500

    fix(MainScene, client/socket/game): recover changes reverted from a previous commit

--- client/scenes/MainScene.js
+++ client/scenes/MainScene.js
@@ -1,100 +1,49 @@
-import "phaser";
-import connect from "../socket";
+import 'phaser';
+import connect from '../socket';
 
 export default class MainScene extends Phaser.Scene {
-<<<<<<< HEAD
-	constructor() {
-		super('MainScene');
-		this.state = {};
-	}
-	preload() {
-		this.load.image('officePlan', 'assets/backgrounds/banner-background.png');
-		// this.load.image('banner', 'assets/backgrounds/banner.png');
-		this.load.image('sprite', 'assets/spritesheets/sprite.png');
-		this.load.image('bear', 'assets/spritesheets/sprite2.png');
-	}
-=======
   constructor() {
-    super("MainScene");
+    super('MainScene');
     this.state = {};
     // Store ids of players overlapping with our sprite
     // Should we move this to a property of scene.state?
     this.nearbyPlayers = {};
   }
   preload() {
-    this.load.image("officePlan", "assets/backgrounds/officePlan.png");
-    this.load.image("banner", "assets/backgrounds/banner.png");
-    this.load.image("sprite", "assets/spritesheets/sprite.png");
-    this.load.image("bear", "assets/spritesheets/sprite2.png");
-    this.load.image("star", "assets/spritesheets/star.png");
+    this.load.image('officePlan', 'assets/backgrounds/officePlan.png');
+    this.load.image('banner', 'assets/backgrounds/banner.png');
+    this.load.image('sprite', 'assets/spritesheets/sprite.png');
+    this.load.image('bear', 'assets/spritesheets/sprite2.png');
+    this.load.image('star', 'assets/spritesheets/star.png');
   }
->>>>>>> 65e4dd663969f834dbbd79c93acdc1b7c8e872e7
 
-	create() {
-		const scene = this;
+  create() {
+    const scene = this;
 
-		this.socket = io();
-		//connect front-end socket listeners
-		connect(scene);
+    this.socket = io();
+    //connect front-end socket listeners
+    connect(scene);
 
-<<<<<<< HEAD
-		//background
-		const background = this.add.image(350, 350, 'officePlan');
-		// const banner = this.add.image(400, 50, 'banner');
-		background.height = game.height;
-		background.width = game.width;
-<<<<<<< HEAD
-=======
-		console.log('I changed the height & width!);
->>>>>>> main
-
-		// CREATE OTHER PLAYERS GROUP
-		this.otherPlayers = this.physics.add.group();
-
-		// Join the game room with roomKey 'office'
-		this.socket.emit('joinRoom', 'office');
-=======
     //background
-    this.add.image(400, 300, "officePlan");
-    const banner = this.add.image(400, 50, "banner");
+    this.add.image(400, 300, 'officePlan');
+    const banner = this.add.image(400, 50, 'banner');
     banner.setScale(0.4);
     // CREATE OTHER PLAYERS GROUP
     this.otherPlayers = this.physics.add.group();
 
     // Join the game room with roomKey 'office'
-    this.socket.emit("joinRoom", "office");
->>>>>>> 65e4dd663969f834dbbd79c93acdc1b7c8e872e7
+    this.socket.emit('joinRoom', 'office');
 
-		//set movement keys to arrow keys
-		this.cursors = this.input.keyboard.createCursorKeys();
+    //set movement keys to arrow keys
+    this.cursors = this.input.keyboard.createCursorKeys();
 
-		//set physics and bounds on the game world
-		this.physics.world.enable(this);
-		this.physics.world.setBounds(0, 0, 800, 600);
-	}
-	update() {
-		const scene = this;
+    //set physics and bounds on the game world
+    this.physics.world.enable(this);
+    this.physics.world.setBounds(0, 0, 800, 600);
+  }
+  update() {
+    const scene = this;
 
-<<<<<<< HEAD
-		//sprite mechanics for movement
-		if (this.sprite) {
-			const speed = 225;
-			const prevVelocity = this.sprite.body.velocity.clone();
-			// Stop any previous movement from the last frame
-			this.sprite.body.setVelocity(0);
-			// Horizontal movement
-			if (this.cursors.left.isDown) {
-				this.sprite.body.setVelocityX(-speed);
-			} else if (this.cursors.right.isDown) {
-				this.sprite.body.setVelocityX(speed);
-			}
-			// Vertical movement
-			if (this.cursors.up.isDown) {
-				this.sprite.body.setVelocityY(-speed);
-			} else if (this.cursors.down.isDown) {
-				this.sprite.body.setVelocityY(speed);
-			}
-=======
     //sprite mechanics for movement
     if (this.sprite) {
       const speed = 225;
@@ -112,53 +61,10 @@ export default class MainScene extends Phaser.Scene {
       } else if (this.cursors.down.isDown) {
         this.sprite.body.setVelocityY(speed);
       }
->>>>>>> 65e4dd663969f834dbbd79c93acdc1b7c8e872e7
 
-			// Normalize and scale the velocity so that sprite can't move faster along a diagonal
-			this.sprite.body.velocity.normalize().scale(speed);
+      // Normalize and scale the velocity so that sprite can't move faster along a diagonal
+      this.sprite.body.velocity.normalize().scale(speed);
 
-<<<<<<< HEAD
-			// emit player movement
-			let x = this.sprite.x;
-			let y = this.sprite.y;
-			if (
-				this.sprite.oldPosition &&
-				(x !== this.sprite.oldPosition.x || y !== this.sprite.oldPosition.y)
-			) {
-				this.moving = true;
-				this.socket.emit('playerMovement', {
-					x: this.sprite.x,
-					y: this.sprite.y,
-					roomKey: scene.state.roomKey,
-				});
-			}
-			// save old position data
-			this.sprite.oldPosition = {
-				x: this.sprite.x,
-				y: this.sprite.y,
-				rotation: this.sprite.rotation,
-			};
-		}
-	}
-	//need to change sprite location to dynamic
-	addPlayer(scene, playerInfo) {
-		scene.joined = true;
-		scene.sprite = scene.physics.add
-			.sprite(playerInfo.x, playerInfo.y, 'sprite')
-			.setScale(0.7)
-			.setVisible(true)
-			.setCollideWorldBounds(true);
-	}
-	addOtherPlayers(scene, playerInfo) {
-		const otherPlayer = scene.physics.add
-			.sprite(playerInfo.x + 40, playerInfo.y + 40, 'sprite')
-			.setScale(0.7)
-			.setVisible(true)
-			.setCollideWorldBounds(true);
-		otherPlayer.playerId = playerInfo.playerId;
-		scene.otherPlayers.add(otherPlayer);
-	}
-=======
       // emit player movement
       let x = this.sprite.x;
       let y = this.sprite.y;
@@ -167,7 +73,7 @@ export default class MainScene extends Phaser.Scene {
         (x !== this.sprite.oldPosition.x || y !== this.sprite.oldPosition.y)
       ) {
         this.moving = true;
-        this.socket.emit("playerMovement", {
+        this.socket.emit('playerMovement', {
           x: this.sprite.x,
           y: this.sprite.y,
           roomKey: scene.state.roomKey,
@@ -195,7 +101,7 @@ export default class MainScene extends Phaser.Scene {
   addPlayer(scene, playerInfo) {
     scene.joined = true;
     scene.sprite = scene.physics.add
-      .sprite(playerInfo.x, playerInfo.y, "sprite")
+      .sprite(playerInfo.x, playerInfo.y, 'sprite')
       .setScale(0.7)
       .setVisible(true)
       .setCollideWorldBounds(true);
@@ -203,7 +109,7 @@ export default class MainScene extends Phaser.Scene {
   }
   addOtherPlayers(scene, playerInfo) {
     const otherPlayer = scene.physics.add
-      .sprite(playerInfo.x + 40, playerInfo.y + 40, "star")
+      .sprite(playerInfo.x + 40, playerInfo.y + 40, 'star')
       .setScale(0.7)
       .setVisible(true)
       .setCollideWorldBounds(true);
@@ -238,7 +144,7 @@ export default class MainScene extends Phaser.Scene {
     ) {
       // code inside this block runs only the first time overlap is triggered betwen player and otherPlayer
       this.nearbyPlayers[otherPlayer.playerId] = otherPlayer;
-      console.log("checking Overlap:", player.playerId, otherPlayer.playerId);
+      console.log('checking Overlap:', player.playerId, otherPlayer.playerId);
     }
   }
   // Check whether there are any nearbyPlayers that we're no longer overlapping with
@@ -257,5 +163,4 @@ export default class MainScene extends Phaser.Scene {
       }
     });
   }
->>>>>>> 65e4dd663969f834dbbd79c93acdc1b7c8e872e7
 }
